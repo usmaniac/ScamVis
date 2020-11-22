8524fff39c6f632965f4442dc69c036194180614
Usman Haidar <usmanhaidar56@gmail.com>
2020-11-22 16:35:18 +1100

    eos_btc not working on bw (removed)

--- FrontEndScamVis/src/components/BWwashCoins.js
+++ FrontEndScamVis/src/components/BWwashCoins.js
@@ -3,7 +3,6 @@ const BWwashCoins = [
     {value: 1, label: "BSV_BTC"},
     {value: 2, label: "ETH_BTC"},
     {value: 3, label: "LTC_BTC"},
-    {value: 4, label: "EOS_BTC"},
     {value: 5, label: "XRP_BTC"},
     {value: 6, label: "ABBC_BTC"},
     {value: 7, label: "BURN_BTC"},
